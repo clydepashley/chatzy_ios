bacdcb18cbcdc4e53d0fa3177c7615fb5bb469af
clyde pashley <clydepashley@hotmail.com>
2015-07-26 14:58:16 +0200

    Version_1_basic chat client

---
+++ MobileDevBootcampChatClient/MobileDevBootcampChatClient-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  MobileDevBootcampChatClient-Bridging-Header.h
+//  MobileDevBootcampChatClient
+//
+//  Created by Pashley Clyde on 7/25/15.
+//  Copyright (c) 2015 DeloitteDigital. All rights reserved.
+//
--- MobileDevBootcampChatClient/ViewController.swift
+++ MobileDevBootcampChatClient/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var chatLabel: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    lazy var socket:SocketIOClient = {
+        let soc = SocketIOClient(socketURL: "mobiledevbootcampchat.mybluemix.net")
+        
+        soc.on("connect") {data, ack in
+            soc.emit("chat message", "\(UIDevice.currentDevice().name) joined the conversation")
+            println("socket connected")
+        }
+        
+        soc.on("chat message") {data, ack in
+            
+            if let message = data?.firstObject as? String {
+                self.chatLabel.text = "\(self.chatLabel.text!)\n\(message)"
+                
+            }
+            
+            /* Example code from Socket.io
+            
+                if let cur = data?[0] as? Double {
+                    soc.emitWithAck("canUpdate", cur)(timeoutAfter: 0) {data in
+                        soc.emit("update", ["amount": cur + 2.50])
+                    }
+                    ack?("Got your currentAmount", "dude")
+                }
+            */
+        }
+
+        return soc
+    }()
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        inputTextField.becomeFirstResponder()
+        socket.connect()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func sendMessage() {
+        socket.emit("chat message", inputTextField.text)
+        inputTextField.text = ""
+        
     }
-
-
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        sendMessage()
+        return false
+    }
+    
+  
 }
 
